a0c8ddf16d7293c6e8af788f2e11a7f1680f70c7
aduoke33 <23010329@st.phenikaa-uni.edu.vn>
2025-06-12 22:59:38 +0700

    Chuyển id users/projects sang UUID, đồng bộ khóa ngoại, migrate lại database

---
+++ app/Models/Project.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Project extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'priority',
+        'status',
+        'deadline',
+    ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_06_12_154731_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('priority', ['Thấp', 'Trung bình', 'Cao'])->default('Trung bình');
+            $table->enum('status', ['Lên kế hoạch', 'Đang thực hiện', 'Đã hoàn thành'])->default('Lên kế hoạch');
+            $table->date('deadline')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
